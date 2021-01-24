f8d74d4ae73137161c71ad61c9428cb3fd1fb6a5
nathb <nathb2b@gmail.com>
2021-01-24 10:07:30 +0100

    ^

---
+++ samples/demo/.layers.js
@@ -0,0 +1,64 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+	"www"   : {
+		"rootFolder": "src",
+		"basedOn"   : "static",
+		"vars"      : {
+			"targetDir"   : "../../docs",
+			"rootAlias"   : "App",
+			"entryPoint"  : "./src",
+			"production"  : true,
+			"babelPreset" : {
+				"targets": {
+					"browsers": "> 0.25%, not dead, safari >= 7, ios_saf >= 7, chrome >= 52"
+				},
+				"loose"  : true
+			},
+			"webpackPatch": {
+				"optimization": {
+					//          "namedModules": true
+				},
+				plugins       : [
+					new CopyPlugin({
+						               patterns: [
+							               { from: "../*/dist/static/**/*", to: "./samples/samples" }
+						               ],
+					               }),
+				],
+				"devtool"     : "source-map"
+			}
+		},
+		"extend"    : [
+			"lpack-react"
+		]
+	},
+	"wwwDev": {
+		"rootFolder": "src",
+		"basedOn"   : "static",
+		"vars"      : {
+			//      "targetDir": "../",
+			"rootAlias"   : "App",
+			"entryPoint"  : "./src",
+			"babelPreset" : {
+				"targets": {
+					"browsers": "> 0.25%, not dead, safari >= 7, ios_saf >= 7, chrome >= 52"
+				},
+				"loose"  : true
+			},
+			"webpackPatch": {
+				"optimization": {
+					//          "namedModules": true
+				},
+				"devServer"   : {
+					"disableHostCheck": true
+				},
+				"devtool"     : "source-map"
+			}
+		},
+		"extend"    : [
+			"lpack-react"
+		]
+	}
+}
