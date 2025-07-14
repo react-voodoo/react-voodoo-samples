614b942ecf3acdda1db8a666f33a7c95b7e68a6e
N8tz <n8tz.js@gmail.com>
2025-07-14 13:07:26 +0200

    ^ fixs

--- samples/Cubes/src/comps/Cube.js
+++ samples/Cubes/src/comps/Cube.js
@@ -28,10 +28,6 @@ import React  from "react";
 import Voodoo from "react-voodoo";
 
 
-/**
- * This is an experimental lib & a very alpha demo
- * Probably not the simpler methods
- */
 
 const initialBallStyle = {
 	position: "absolute",
@@ -43,226 +39,217 @@ const initialBallStyle = {
 	
 };
 
-@Voodoo.tweener({ enableMouseDrag: true })
-export default class Cube extends React.Component {
-	root = React.createRef();
+export default ( props ) => {
+	const [tweener, ViewBox] = Voodoo.hook({ enableMouseDrag: true }),
+	      {
+		      defaultPosition = {
+			      x: .5,
+			      y: .5
+		      },
+		      style           = initialBallStyle,
+		      color           = "black"
+	      }                  = props,
+	      styles             = React.useMemo(
+		      () => (
+			      {
+				      facesStyle  : {
+					      front : {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateY: 0, translateZ: "6vh" }
+					      },
+					      right : {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateY: 90, translateZ: "6vh" }
+					      },
+					      back  : {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateY: 180, translateZ: "6vh" }
+					      },
+					      left  : {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateY: -90, translateZ: "6vh" }
+					      },
+					      top   : {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateX: 90, translateZ: "6vh" }
+					      },
+					      bottom: {
+						      position       : "absolute",
+						      width          : "100%",
+						      height         : "100%",
+						      backgroundColor: color,
+						      opacity        : .5,
+						      transform      : { rotateX: -90, translateZ: "6vh" }
+					      }
+				      },
+				      cubeStyle   : {
+					      position      : "absolute",
+					      width         : "10vh",
+					      height        : "10vh",
+					      left          : (defaultPosition.x * 100) + "%",
+					      top           : (defaultPosition.y * 100) + "%",
+					      transformStyle: "preserve-3d",
+					      transform     : [
+						      {
+							      translateX: "-50%",
+							      translateY: "-50%",
+							      rotateX   : "-20deg",
+						      },
+					      ]
+				      },
+				      contentStyle: {
+					      position : "absolute",
+					      width    : "8vh",
+					      height   : "8vh",
+					      left     : "50%",
+					      top      : "50%",
+					      transform: [
+						      {
+							      translateX: "-50%",
+							      translateY: "-50%",
+						      }, {
+							      //rotateX: "20deg",
+						      },
+					      ]
+				      },
+				      axis        : {
+					      scrollX: [
+						      {
+							      from    : 0,
+							      duration: 300,
+							      target  : "root",
+							      apply   : {
+								      transform: [, {
+									      rotateY: "-1080deg"
+								      }],
+							      }
+						      },
+						      {
+							      from    : 0,
+							      duration: 300,
+							      target  : "content",
+							      apply   : {
+								      transform: [, {
+									      rotateY: "1080deg"
+								      }],
+							      }
+						      }
+					      ],
+					      scrollY: [
+						      ...['front', 'back', 'left', 'right', 'top'].map(
+							      target => ({
+								      from    : 0,
+								      duration: 100,
+								      target,
+								      apply   : {
+									      transform: [{}, {
+										      translateZ: "1vh",
+									      }],
+								      }
+							      })
+						      ),
+						      ...['front', 'back', 'left', 'right'].map(
+							      target => ({
+								      from    : 10,
+								      duration: 100,
+								      target,
+								      apply   : {
+									      transform: [{}, {
+										      translateZ: "4vh",
+										      translateY: "2vh",
+										      rotateX   : "-45deg",
+									      }],
+								      }
+							      })
+						      ),
+						      {
+							      from    : 10,
+							      duration: 90,
+							      target  : "top",
+							      apply   : {
+								      opacity  : -.5,
+								      transform: [, {
+									      translateZ: "9vh",
+									      scale     : -.5,
+								      }],
+							      }
+						      },
+						      {
+							      from    : 10,
+							      duration: 190,
+							      target  : "content",
+							      apply   : {
+								      width    : "3vh",
+								      height   : "3vh",
+								      transform: [, , {
+									      translateY: "-15vh",
+									      
+								      }],
+							      }
+						      }
+					      ]
+				      },
+				      inertiaX    : {
+					      snapToBounds: false,
+					      shouldLoop  : (( v, d ) => {
+						      if ( d < 0 && ~~(d + v) <= 100 ) {
+							      return 100;
+						      }
+						      if ( d > 0 && ~~(d + v) >= 200 ) {
+							      return -100;
+						      }
+					      }),
+				      },
+				      inertiaY    :
+					      {
+						      wayPoints: [{ at: 10 }, { at: 200 }]
+					      }
+			      }
+		      ), [color, style, defaultPosition]
+	      );
 	
-	static defaultProps = {
-		defaultPosition: {
-			x: .5,
-			y: .5
-		},
-		style          : initialBallStyle,
-		color          : "black"
-	};
-	state               = {};
-	nextTarget          = {};
 	
-	
-	static getDerivedStateFromProps( props, state ) {
-		let { color, style, defaultPosition } = props;
-		return {
-			facesStyle  : {
-				front : {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateY: 0, translateZ: "6vh" }
-				},
-				right : {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateY: 90, translateZ: "6vh" }
-				},
-				back  : {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateY: 180, translateZ: "6vh" }
-				},
-				left  : {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateY: -90, translateZ: "6vh" }
-				},
-				top   : {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateX: 90, translateZ: "6vh" }
-				},
-				bottom: {
-					position       : "absolute",
-					width          : "100%",
-					height         : "100%",
-					backgroundColor: color,
-					opacity        : .5,
-					transform      : { rotateX: -90, translateZ: "6vh" }
-				}
-			},
-			cubeStyle   : {
-				position      : "absolute",
-				width         : "10vh",
-				height        : "10vh",
-				left          : (defaultPosition.x * 100) + "%",
-				top           : (defaultPosition.y * 100) + "%",
-				transformStyle: "preserve-3d",
-				transform     : [
-					{
-						translateX: "-50%",
-						translateY: "-50%",
-						rotateX   : "-20deg",
-					},
-				]
-			},
-			contentStyle: {
-				position : "absolute",
-				width    : "8vh",
-				height   : "8vh",
-				left     : "50%",
-				top      : "50%",
-				transform: [
-					{
-						translateX: "-50%",
-						translateY: "-50%",
-					}, {
-						//rotateX: "20deg",
-					},
-				]
-			},
-			axis        : {
-				scrollX: [
-					{
-						from    : 0,
-						duration: 300,
-						target  : "root",
-						apply   : {
-							transform: [, {
-								rotateY: "-1080deg"
-							}],
-						}
-					},
-					{
-						from    : 0,
-						duration: 300,
-						target  : "content",
-						apply   : {
-							transform: [, {
-								rotateY: "1080deg"
-							}],
-						}
-					}
-				],
-				scrollY: [
-					...['front', 'back', 'left', 'right', 'top'].map(
-						target => ({
-							from    : 0,
-							duration: 100,
-							target,
-							apply   : {
-								transform: [{}, {
-									translateZ: "1vh",
-								}],
-							}
-						})
-					),
-					...['front', 'back', 'left', 'right'].map(
-						target => ({
-							from    : 10,
-							duration: 100,
-							target,
-							apply   : {
-								transform: [{}, {
-									translateZ: "4vh",
-									translateY: "2vh",
-									rotateX   : "-45deg",
-								}],
-							}
-						})
-					),
-					{
-						from    : 10,
-						duration: 90,
-						target  : "top",
-						apply   : {
-							opacity  : -.5,
-							transform: [, {
-								translateZ: "9vh",
-								scale     : -.5,
-							}],
-						}
-					},
-					{
-						from    : 10,
-						duration: 190,
-						target  : "content",
-						apply   : {
-							width    : "3vh",
-							height   : "3vh",
-							transform: [, , {
-								translateY: "-15vh",
-								
-							}],
-						}
-					}
-				]
-			}
-		}
-	}
-	
-	inertiaX = {
-		snapToBounds: false,
-		shouldLoop  : (( v, d ) => {
-			if ( d < 0 && ~~(d + v) <= 100 ) {
-				return 100;
-			}
-			if ( d > 0 && ~~(d + v) >= 200 ) {
-				return -100;
-			}
-		}),
-	};
-	inertiaY =
-		{
-			wayPoints: [{ at: 10 }, { at: 200 }]
-		};
-	
-	render() {
-		let { facesStyle, cubeStyle, contentStyle, axis } = this.state;
-		return <>
-			<Voodoo.Axis axe={"scrollY"} defaultPosition={10}
-			             items={axis.scrollY}
-			             inertia={this.inertiaY}/>
-			<Voodoo.Axis axe={"scrollX"} defaultPosition={100 + 30}
-			             items={axis.scrollX}
-			             scrollableWindow={33}
-			             inertia={this.inertiaX}/>
-			<Voodoo.Node
-				style={cubeStyle}
-				id={"root"}>
-				
-				<Voodoo.Draggable yAxis={"scrollY"} xAxis={"scrollX"}>
-					<Voodoo.Node.div id={"front"} initial={facesStyle.front} className={"face"}/>
-					<Voodoo.Node.div id={"back"} initial={facesStyle.back} className={"face"}/>
-					<Voodoo.Node.div id={"right"} initial={facesStyle.right} className={"face"}/>
-					<Voodoo.Node.div id={"left"} initial={facesStyle.left} className={"face"}/>
-					<Voodoo.Node.div id={"top"} initial={facesStyle.top} className={"face"}/>
-					<Voodoo.Node.div id={"bottom"} initial={facesStyle.bottom} className={"face"}/>
-					<Voodoo.Node.div id={"content"} initial={contentStyle} className={"content"}/>
-				
-				</Voodoo.Draggable>
-			</Voodoo.Node>
-		</>;
-	}
+	return <ViewBox className={"Cube"}>
+		<Voodoo.Axis axe={"scrollY"} defaultPosition={10}
+		             items={styles.axis.scrollY}
+		             inertia={styles.inertiaY}/>
+		<Voodoo.Axis axe={"scrollX"} defaultPosition={100 + 30}
+		             items={styles.axis.scrollX}
+		             scrollableWindow={33}
+		             inertia={styles.inertiaX}/>
+		<Voodoo.Node
+			style={styles.cubeStyle}
+			id={"root"}>
+			
+			<Voodoo.Draggable yAxis={"scrollY"} xAxis={"scrollX"}>
+				<Voodoo.Node.div id={"front"} initial={styles.facesStyle.front} className={"face"}/>
+				<Voodoo.Node.div id={"back"} initial={styles.facesStyle.back} className={"face"}/>
+				<Voodoo.Node.div id={"right"} initial={styles.facesStyle.right} className={"face"}/>
+				<Voodoo.Node.div id={"left"} initial={styles.facesStyle.left} className={"face"}/>
+				<Voodoo.Node.div id={"top"} initial={styles.facesStyle.top} className={"face"}/>
+				<Voodoo.Node.div id={"bottom"} initial={styles.facesStyle.bottom} className={"face"}/>
+				<Voodoo.Node.div id={"content"} initial={styles.contentStyle} className={"content"}/>
+			</Voodoo.Draggable>
+		</Voodoo.Node>
+	</ViewBox>;
 }
